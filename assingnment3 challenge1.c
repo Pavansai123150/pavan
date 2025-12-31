3bc5c36238212c1c8d3d8aa763af967116a6dd89
Pavansai123150 <avupatipavansai@gmail.com>
2025-12-31 15:34:02 +0530

    assignment 3 challenge1

---
+++ assingnment3 challenge1.c
@@ -0,0 +1,18 @@
+// Include the standard input-output library to use printf
+#include <stdio.h>
+
+// Main function where program execution begins
+int main() {
+
+    // Print the name
+    printf("Name: Alex\n");
+
+    // Print the age
+    printf("Age: 20\n");
+
+    // Print the country
+    printf("Country: India\n");
+
+    // End the program successfully
+    return 0;
+}
